ccfd029792d89ba581c6b7ca7aa14619d3db04d0
Anne Belka and Brian Pritt <anne+brian@>
2016-11-16 09:16:29 -0800

    add id and RESTful routing

--- HomeModules.cs
+++ HomeModules.cs
@@ -1,6 +1,6 @@
 using Nancy;
-using ToDoList.Objects;
 using System.Collections.Generic;
+using ToDoList.Objects;
 
 namespace ToDoList
 {
@@ -8,19 +8,24 @@ namespace ToDoList
   {
     public HomeModule()
     {
-      Get["/"] = _ => View["add_new_task.cshtml"];
-      Get["/view_all_tasks"] = _ => {
-        List<string> allTasks = Task.GetAll();
-        return View["view_all_tasks.cshtml", allTasks];
+      Get["/"] = _ => {
+        return View["index.cshtml"];
       };
-      Post["/task_added"] = _ => {
-        Task newTask = new Task (Request.Form["new-task"]);
-        newTask.Save();
-        return View["task_added.cshtml", newTask];
+      Get["/tasks"] = _ => {
+        List<Task> allTasks = Task.GetAll();
+        return View["tasks.cshtml", allTasks];
       };
-      Post["/tasks_cleared"] = _ => {
-        Task.ClearAll();
-        return View["tasks_cleared.cshtml"];
+      Get["/tasks/new"] = _ => {
+        return View["task_form.cshtml"];
+      };
+      Get["/tasks/{id}"] = parameters => {
+        Task task = Task.Find(parameters.id);
+        return View["/task.cshtml", task];
+      };
+      Post["/tasks"] = _ => {
+        Task newTask = new Task(Request.Form["new-task"]);
+        List<Task> allTasks = Task.GetAll();
+        return View["tasks.cshtml", allTasks];
       };
     }
   }
--- Objects/Tasks.cs
+++ Objects/Tasks.cs
@@ -5,33 +5,40 @@ namespace ToDoList.Objects
   public class Task
   {
     private string _description;
-    private static List<string> _instances = new List<string> {};
+    private int _id;
+    private static List<Task> _instances = new List<Task> {};
 
-    public Task (string description)
+    public Task(string description)
     {
       _description = description;
+      _instances.Add(this);
+      _id = _instances.Count;
     }
 
     public string GetDescription()
     {
       return _description;
     }
+
     public void SetDescription(string newDescription)
     {
       _description = newDescription;
     }
-
-    public static List<string> GetAll()
+    public int GetId()
+    {
+      return _id;
+    }
+    public static List<Task> GetAll()
     {
       return _instances;
-    }
-    public void Save()
-    {
-      _instances.Add(_description);
     }
     public static void ClearAll()
     {
       _instances.Clear();
+    }
+    public static Task Find(int searchId)
+    {
+      return _instances[searchId-1];
     }
   }
 }
